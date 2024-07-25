174a532d81e5c10a702c4a7cb216f8c08a1f3433
rajat <fzdsharmarajat@gmail.com>
2024-07-25 20:19:28 +0530

    somechanges

--- POCbackend/db/db.js
+++ POCbackend/db/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-const mongoUri ='mongodb+srv://fzdsharmarajat:8445234607@cluster0.fmkq5bm.mongodb.net/userdata?'
-const connection = mongoose.connect(mongoUri)
+const connection = mongoose.connect(mongoUri `${process.env.mongoUri}`)
 .then(() => {
     console.log('Connected to MongoDB');
 })
--- POCbackend/server.js
+++ POCbackend/server.js
@@ -3,7 +3,6 @@ const app = express();
 //const connection = require("./db/db");
 const connection = require("./db/mysqldb");
 const redisClient = require("./db/redisdb");
-const port = 4000;
 const cors = require("cors");
 connection;
 
@@ -13,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", require("./routes/userroutes"));
 
-app.listen(port, () => {
-  console.log(`port listening on ${port}`);
+app.listen(process.env.port || 4000, () => {
+  console.log(`port listening on ${process.env.port}`);
 });
